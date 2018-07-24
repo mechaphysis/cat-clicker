e073c313dc4d93ef33ca2c958d50ad947fccf19d
mechaphysis <triptofante@gmail.com>
2018-07-24 17:41:01 +0200

    feat: Add next iteration of requirements

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
-let counter = document.querySelector('.counter');
+let counters = [document.querySelector('.counter-origin'),document.querySelector('.counter-heidi')];
 let count = 0;
-let cat = document.querySelector('.cat');
+let cats = [document.querySelector('.cat-origin'),document.querySelector('.cat-heidi')];
 
+
+cats[0].prepend('Origin Cat');
+cats[1].prepend('Heidi Cat');
 //Define function for incrementing the counter of moves
 function incremCounter(){
   count += 1;
-  counter.innerText = count +' clicks!';
+  for (counter of counters) {
+    counter.innerText = count +' clicks!';
+  };
 };
 
-cat.addEventListener('click', function(evt) {
-  incremCounter();
-})
+
+for (cat of cats) {
+  cat.addEventListener('click', function() {
+    incremCounter();
+  });
+};
